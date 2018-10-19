4aab6f10c45d2d5ab68d3decd12ea6f509397d68
soga0901 <sg.0901.sg@gmail.com>
2018-10-19 10:06:58 +0900

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <!-- <HelloWorld/> -->
+    <Soga />
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld';
+import Soga from './components/Soga';
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    Soga,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Soga.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>FuckYouWorld</div>
+</template>
+
+<script>
+export default {
+  name: 'Soga',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
