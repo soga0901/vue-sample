59fc19bdc991363fdec7d65ef34df1c5c9c5bc87
soga0901 <sg.0901.sg@gmail.com>
2018-10-22 09:59:48 +0900

    :sparkles: Create Header

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,83 @@
 <template>
-  <div>
-    <p>
-      {{ msg }}
-    </p>
-    <input type="text" v-model="msg">
+  <div class="App">
+    <DefaultHeader/>
+    <div class="Contents">
+      <p v-if="msg.length > 0">
+        {{ msg }}
+      </p>
+      <p v-else>
+        no text
+      </p>
+      <input type="text" v-model="msg">
+      <button @click="clear()">clear</button>
+    </div>
   </div>
 </template>
 <script>
+import DefaultHeader from './components/DefaultHeader';
 
 export default {
-  data () {
+  components: {
+    DefaultHeader,
+  },
+  data() {
     return {
       msg: 'input something.',
-    }
-  }
-}
+    };
+  },
+  methods: {
+    clear() {
+      this.msg = '';
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+.Contents {
+  padding-top: 60px;
 }
 </style>
---
+++ src/components/DefaultHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Header">
+    Header
+  </div>
+</template>
+<style>
+.Header {
+  background-color: #f1f1f1;
+  position: fixed;
+  width: 100%;
+  height: 60px;
+}
+</style>
